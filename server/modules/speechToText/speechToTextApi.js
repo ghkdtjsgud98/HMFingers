60b41b3307c7f4567d356f29ad4236fb74c295a1
seungjiKim <necky10000@gmail.com>
2021-11-23 19:05:33 +0900

    Add google cloud speechToTextApi - temp

---
+++ server/modules/speechToText/speechToTextApi.js
@@ -0,0 +1,11 @@
+import speech from '@google-cloud/speech'
+import fs from 'fs'
+
+/* async function convertAudioToScript() {
+  const client = new speech.SpeechClient();
+  const file = './resources/audio.raw' // 없는 파일임.
+
+  const file = fs.readFileSync(filename);
+  const au
+}
+*/
