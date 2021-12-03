9f2027ab320efd65680d9196443446636fb112d7
JIWON_MIN <mjw3534@gmail.com>
2021-12-03 11:55:02 +0900

    Add PlayBar function about TimeStamp
    타임스탬프 관련 재생바 기능 구현 완료

--- client/src/components/MainPage/PlayBar.js
+++ client/src/components/MainPage/PlayBar.js
@@ -6,18 +6,21 @@ import {
 
 const PlayBar = (currentTime2) => {
 
-  console.log("PB-currentTime2 : ", currentTime2);
+// console.log("PB-currentTime2 : ", currentTime2.currentTime2);
+
+var obj = document.getElementById('audio');
+
+if (obj !== null){
+  obj.currentTime = currentTime2.currentTime2;
+}
 
   return (
     <>
 
     <PlayBarBox>
- 
-    {/* currentTime = {currentTime} */}
-      <audio currentTime={currentTime2} id="audio" preload="metadata" style={{width : "100%", height : "100%", border : "none", borderRadius : "30px"}} controls loop>
+      <audio id="audio" preload="metadata" style={{width : "100%", height : "100%", border : "none", borderRadius : "30px"}} controls loop>
         <source id="sndSrc" src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_5MG.mp3" type="audio/mpeg" />
           {/* src에 상대경로로 파일 넣으면 왜 실행이 안되지? */}
-        Your browser does not support the audio element.
       </audio>
     </PlayBarBox>
 
--- client/src/screens/MainPage.js
+++ client/src/screens/MainPage.js
@@ -7,20 +7,20 @@ import PlayBar from '../components/MainPage/PlayBar';
 
 const MainPage = () => {
 
-  var [currentTime2, setCurrentTime2] = useState();
+  var [currentTime2, setCurrentTime2] = useState(0);
 
   const getTime2 = (t) => {
     setCurrentTime2(t);
-    console.log("MP-getTime2 작동o");
+    // console.log("MP-getTime2 작동o");
   }
 
-  console.log("MP-currentTime2 : ", currentTime2);
+  // console.log("MP-currentTime2 : ", currentTime2.t);
 
   return (
     <>
       <UpperBar />
       <Output getTime2={getTime2} />
-      <PlayBar currentTime2={currentTime2} />
+      <PlayBar currentTime2={currentTime2.t} />
       <KeywordSearch />
       <FileHandling />
     </>
