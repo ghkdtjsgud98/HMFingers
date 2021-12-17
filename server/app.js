e1dd7e48d4f791f298c29698e4356852273f70b6
황선형 <ghkdtjsgud98@naver.com>
2021-12-17 15:47:53 +0900

    cors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import home from "./src/routes/home/index.js";
 import path from "path";
+import cors from "cors";
 
 //모듈
 const app = express();
@@ -12,6 +13,7 @@ const __dirname = path.resolve();
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(cors());
 app.use(express.static(`${__dirname}/src/public`));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
