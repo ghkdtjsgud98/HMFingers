7a3f59f48ee25aa8733e40d196942b8b06e1ab5d
황선형 <ghkdtjsgud98@naver.com>
2021-12-18 21:23:58 +0900

    임시 ui 삭제

--- server/src/routes/home/index.js
+++ server/src/routes/home/index.js
@@ -3,8 +3,8 @@ import express from "express";
 import ctrl from "./home.ctrl.js";
 const router = express.Router();
 
-router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/", ctrl.output.home);
 router.get("/register", ctrl.output.register);
 
 router.post("/login", ctrl.process.login);
