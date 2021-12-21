afaa4919b24a5295bb42bd1d6a627427579a754c
황선형 <ghkdtjsgud98@naver.com>
2021-12-22 00:49:48 +0900

    번역 api

---
+++ server/modules/translation/translateApi.js
@@ -0,0 +1,28 @@
+import Translate from "@google-cloud/translate";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Translate.v2.Translate({
+  projectId: "strategic-hull-331715",
+  keyFilename: "/Users/user/Downloads/strategic-hull-331715-417b23f22a38.json",
+});
+
+export async function translation(text) {
+  let input = "";
+  var [detections] = await client.detect(text);
+  detections = Array.isArray(detections) ? detections : [detections];
+  console.log("Detections:");
+  detections.forEach((detection) => {
+    console.log(`${detection.input} => ${detection.language}`);
+    input = detection.language;
+    //return detection.language;
+  });
+
+  var [translations] = await client.translate(text, input);
+  translations = Array.isArray(translations) ? translations : [translations];
+  console.log("Translations:");
+  translations.forEach((translation, i) => {
+    console.log(`${text} => (${input}) ${translation}`);
+  });
+}
