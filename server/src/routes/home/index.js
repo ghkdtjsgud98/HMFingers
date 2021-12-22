b154094e458e5da852bcf331d06fd4b412e6b339
황선형 <ghkdtjsgud98@naver.com>
2021-12-23 02:16:48 +0900

    파일저장,파일리스트 불러오기 등등

--- server/src/routes/home/index.js
+++ server/src/routes/home/index.js
@@ -6,13 +6,23 @@ import ctrl from "./home.ctrl.js";
 
 const router = express.Router();
 const __dirname = path.resolve();
-const multipartMiddleware = multiparty({ uploadDir:__dirname + '/resources'})
+const multipartMiddleware = multiparty({ uploadDir: __dirname + "/resources" });
 
 router.get("/login", ctrl.output.login);
 router.get("/", ctrl.output.home);
 router.get("/register", ctrl.output.register);
+router.get("/getlist", async (req, res) => await ctrl.output.getlist(req, res));
+router.get(
+  "/getfile",
+  multipartMiddleware,
+  async (req, res) => await ctrl.output.getfile(req, res)
+);
 
 router.post("/login", ctrl.process.login);
 router.post("/register", ctrl.process.register);
-router.post("/uploadAudio", multipartMiddleware, async (req, res) => await ctrl.process.uploadAudio(req, res));
+router.post(
+  "/uploadAudio",
+  multipartMiddleware,
+  async (req, res) => await ctrl.process.uploadAudio(req, res)
+);
 export default router;
